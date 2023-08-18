e32997a731528dbf74c57005e85b360ab64da2e8
Lucas Vaz <lucasalvesvaz@hotmail.com>
2023-08-18 17:56:11 -0300

    feat: added create table address script

--- question4/Application.sql
+++ question4/Application.sql
@@ -1,2 +1,11 @@
 CREATE DATABASE challenge;
-
+CREATE TABLE IF NOT EXISTS address (
+	address_id SERIAL PRIMARY KEY,
+	zip VARCHAR (10) NOT NULL,
+	street VARCHAR (50),
+	complement VARCHAR (50) NOT NULL,
+	neighborhood VARCHAR (50),
+	city VARCHAR (50) NOT NULL,
+	state VARCHAR (50),
+	country VARCHAR (50)
+);
