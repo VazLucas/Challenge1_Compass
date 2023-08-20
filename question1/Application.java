3a0c7ada9cffb2d5336a006fe40e8bd6c5b4d968
Lucas Vaz <lucasalvesvaz@hotmail.com>
2023-08-20 03:01:51 -0300

    feat: start question production

---
+++ question1/Application.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Application {
+
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> problemSolved = new ArrayList<Integer>();
+        ArrayList<String> studentsName = new ArrayList<String>();
+    }
+
+}
+
+
