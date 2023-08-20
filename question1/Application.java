4ea780a2ddf452c2804e66108842d2e3cd675335
Lucas Vaz <lucasalvesvaz@hotmail.com>
2023-08-20 03:06:04 -0300

    feat: added loop to compare names alphabetically

--- question1/Application.java
+++ question1/Application.java
@@ -15,23 +15,33 @@ public class Application {
             studentsName.add(source.next());
             problemSolved.add(source.nextInt());
         }
-        for (int j = problemSolved.size()-1, i = 0; j >0;j-- ) {
-            if(problemSolved.get(i)>problemSolved.get(j))
-            {
+        for (int j = problemSolved.size() - 1, i = 0; j > 0; j--) {
+            if (problemSolved.get(i) > problemSolved.get(j)) {
                 problemSolved.remove(i);
                 studentsName.remove(i);
-            }
-            else if (problemSolved.get(i)<problemSolved.get(j)){
+            } else if (problemSolved.get(i) < problemSolved.get(j)) {
                 problemSolved.remove(j);
                 studentsName.remove(j);
 
-            }
-            else if (Objects.equals(problemSolved.get(i), problemSolved.get(j))){
+            } else if (Objects.equals(problemSolved.get(i), problemSolved.get(j))) {
+                if (studentsName.get(i).compareTo(studentsName.get(j)) > 0) {
+                    studentsName.remove(j);
+                    problemSolved.remove(j);
+                } else if (studentsName.get(i).compareTo(studentsName.get(j)) < 0) {
+                    studentsName.remove(i);
+                    problemSolved.remove(i);
+                } else {
+                    System.out.println("Both students have the same name");
+                    break;
+                }
 
+            } else {
+                break;
             }
+
+        }
 
     }
-
 }
 
 
