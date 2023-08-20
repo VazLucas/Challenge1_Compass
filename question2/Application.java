825a6d3a43003dd181b62b90bfa84000e844fd8a
Lucas Vaz <lucasalvesvaz@hotmail.com>
2023-08-20 15:00:35 -0300

    feat: added scanner class and populated array

--- question2/Application.java
+++ question2/Application.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 
 public class Application {
     public static void main (String []args) {
-
+        Scanner source = new Scanner(System.in);
+        String line = source.nextLine();
+        String[] elements = line.split(" ");
+        int upsetCount = 0, funCount = 0;
 
     }
 }
