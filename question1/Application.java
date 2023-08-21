f682d9cd00f249c43f491634911dccfd0466d058
Lucas Vaz <lucasalvesvaz@hotmail.com>
2023-08-21 15:14:07 -0300

    feat: added loop structure

--- question1/Application.java
+++ question1/Application.java
@@ -5,43 +5,50 @@ public class Application {
 
     public static void main(String[] args) {
 
-        ArrayList<Integer> problemSolved = new ArrayList<Integer>();
-        ArrayList<String> studentsName = new ArrayList<String>();
-
+        List<Integer> problemSolved = new ArrayList<Integer>();
+        List<String> studentsName = new ArrayList<String>();
+        int k = 1;
         Scanner source = new Scanner(System.in);
-        int studentsQuantity = source.nextInt();
 
-        for (int i = 0; i < studentsQuantity; i++) {
-            studentsName.add(source.next());
-            problemSolved.add(source.nextInt());
-        }
-        for (int j = problemSolved.size() - 1, i = 0; j > 0; j--) {
-            if (problemSolved.get(i) > problemSolved.get(j)) {
-                problemSolved.remove(i);
-                studentsName.remove(i);
-            } else if (problemSolved.get(i) < problemSolved.get(j)) {
-                problemSolved.remove(j);
-                studentsName.remove(j);
+        while (source.hasNext()){
 
-            } else if (Objects.equals(problemSolved.get(i), problemSolved.get(j))) {
-                if (studentsName.get(i).compareTo(studentsName.get(j)) > 0) {
-                    studentsName.remove(j);
-                    problemSolved.remove(j);
-                } else if (studentsName.get(i).compareTo(studentsName.get(j)) < 0) {
-                    studentsName.remove(i);
+            int studentsQuantity = source.nextInt();
+            studentsName.clear();
+            problemSolved.clear();
+
+            for (int i = 0; i < studentsQuantity; i++) {
+                studentsName.add(source.next());
+                problemSolved.add(source.nextInt());
+            }
+            for (int j = problemSolved.size() - 1, i = 0; j > 0; j--) {
+                if (problemSolved.get(i) > problemSolved.get(j)) {
                     problemSolved.remove(i);
+                    studentsName.remove(i);
+                } else if (problemSolved.get(i) < problemSolved.get(j)) {
+                    problemSolved.remove(j);
+                    studentsName.remove(j);
+
+                } else if (Objects.equals(problemSolved.get(i), problemSolved.get(j))) {
+                    if (studentsName.get(i).compareTo(studentsName.get(j)) > 0) {
+                        studentsName.remove(j);
+                        problemSolved.remove(j);
+                    } else if (studentsName.get(i).compareTo(studentsName.get(j)) < 0) {
+                        studentsName.remove(i);
+                        problemSolved.remove(i);
+                    } else {
+                        System.out.println("Both students have the same name");
+                        break;
+                    }
+
                 } else {
-                    System.out.println("Both students have the same name");
                     break;
                 }
 
-            } else {
-                break;
             }
-
+            System.out.println("Instance:" + k);
+            System.out.println(studentsName.get(0));
+            k++;
         }
-        System.out.println("Instance:" + k);
-        System.out.println(studentsName.get(0));
 
     }
 }
