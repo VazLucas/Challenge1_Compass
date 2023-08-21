58828f5a9e1d5f4f8603dbc131d2051745c9e842
Lucas Vaz <lucasalvesvaz@hotmail.com>
2023-08-21 15:20:18 -0300

    feat: added loop structure and operands constraints

--- question3/Application.java
+++ question3/Application.java
@@ -8,5 +8,23 @@ public class Application {
         int result = 0;
         List<Integer> results = new ArrayList<>();
         int k = 1;
+
+        while (source.hasNext()) {
+
+            int operandsQuantity = source.nextInt();
+
+            if (operandsQuantity < 1 || operandsQuantity > 100) {
+                break;
+            }
+            if (operandsQuantity != 0) {
+                String equation = source.next();
+
+                String equationFormatted = equation.replaceAll("[0-9]", "");
+                ArrayList equationCounter = new ArrayList<>((List.of(equation.split("[^0-9]"))));
+                ArrayList equationOperator = new ArrayList<>();
+                equationOperator.add("");
+                for (int i = 0; i < equationFormatted.length(); i++) {
+                    equationOperator.add(equationFormatted.charAt(i));
+                }
     }
 }
